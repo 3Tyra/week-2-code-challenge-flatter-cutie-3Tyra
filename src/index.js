4ee2ce16aef098c0fc171d2243b123e0f78c75f6
Tyra <tyra.mwai@student.moringaschool.com>
2025-03-24 09:49:15 +0300

    Added Changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+   });
+  
+ fetchCharacters();
+ function fetchCharacters (){     
+     fetch("http://localhost:3000/characters");
+     then((response) => response.json());
+ }
+
+characters.forEach((character) => {
+    const span = document.createElement("span");  
+    span.textContent = character.name;             
+    span.classList.add("character");               
+   characterBar.appendChild(span);                
+  });
+  
